117032e903144e8c0ccf21045c789ec179650c42
henesy <henesy.dev@gmail.com>
2020-04-15 01:43:57 -0700

    basic a=b works ☺

--- test.adb
+++ test.adb
@@ -1,17 +1,19 @@
-# This is a person
+# A record with 4 tuples and 5 pairs
 type=person
-	name='s e a n'
+	name='Ана'
 	age=10
     food='P is for potato' type="starch"
 
+# A record with 4 tuples of 1 pair each
 quoting=
     quot='foo '' bar'
     dquot="foo ' bar"
     equot='foo " bar'
 
-# This is a rectangle
-type=rectangle height=4 width=6 name='A'
+# A record consisting of a single tuple and 4 pairs
+type=rectangle height=4 width=6		name='A'
 
+# A record consisting of 3 tuples and 4 pairs
 type=rectangle name='B'
     height=9
 	width=7
